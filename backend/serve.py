c875f38d0079db68b117ffe6700ce91f1dfca266
Moises Trejo <moisest@mit.edu>
2021-05-01 14:40:30 -0500

    added edit inputs

--- backend/serve.py
+++ backend/serve.py
@@ -1,5 +1,6 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask_cors import CORS
+import json
 import random
 
 app = Flask(__name__)
@@ -13,9 +14,15 @@ def main():
     }
     return jsonify(response)
 
-@app.route('/derender')
+@app.route('/derender', methods = ['GET', 'POST'])
 def derender():
     try:
+        data = json.loads(request.data)
+        image_url = data['image']
+        print(image_url)
+        # run functions
+
+        # return stats
         stats = []
         for i in range(10):
             stats.append({
@@ -28,8 +35,25 @@ def derender():
         }
         return jsonify(response)
     except Exception as e:
+        print(e)
         return jsonify({"error": e}), 400
 
+@app.route('/changes', methods = ['GET', 'POST'])
+def changes():
+    try:
+        data = json.loads(request.data)
+        edits = data['changes']
+        print(edits)
+
+        # do edits and return an image?
+        
+        response = {
+            "message": "Your image"
+        }
+        return jsonify(response)
+    except Exception as e:
+        print(e)
+        return jsonify({"error": e}), 400
 
 if __name__ == "__main__":
     app.run(debug=True)
--- extension/newpage.js
+++ extension/newpage.js
@@ -1,5 +1,6 @@
 
 let sharedData;
+let imageDerender = {}
 try {
     sharedData = JSON.parse(localStorage.sharedData);
     document.getElementById("image").src = sharedData.info.srcUrl;
@@ -10,44 +11,77 @@ try {
 
 } 
 catch (e) {}
+
 derenderImage = () => {
-    fetch('http://localhost:5000/derender')
+    fetch('http://localhost:5000/derender', 
+        {
+            method: 'POST', // or 'PUT'
+            headers: {
+            'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({'image': sharedData.info.srcUrl}),
+        })
         .then(response => response.json())
         .then(data => {
             
             for( let i = 0 ; i < data.stats.length ; i ++){
                 stat = data.stats[i]
+                imageDerender[stat.text] = stat.text
                 addTooltip(stat.text, stat.x, stat.y)
             }
+        });
+}
+
+submitChanges = () => {
+    fetch('http://localhost:5000/changes', 
+        {
+            method: 'POST', // or 'PUT'
+            headers: {
+            'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({'changes': imageDerender}),
+        })
+        .then(response => response.json())
+        .then(data => {
+            
         });
 }
 
 addTooltip = (text, x, y) => {
     var imageContainer = document.getElementById("image-container");
 
-    var div = document.createElement("div");
-    div.id = 'text-tooltip';
-    div.style.width = '30px'; 
-    div.style.height = '30px';
-    div.style.left = `${x}px`;
-    div.style.top = `${y}px`; 
-    console.log(div.style) 
+    var input = document.createElement("input");
+    input.type = "text";
+    input.value = text;
+    input.id = 'text-tooltip';
+    input.style.width = '30px'; 
+    input.style.height = '10px';
+    input.style.left = `${x}px`;
+    input.style.top = `${y}px`; 
 
-    var node = document.createTextNode(text);
-    div.appendChild(node);
+    input.addEventListener('input', (e) => {
+        console.log(text, '=>', e.target.value);
+        imageDerender[text] = e.target.value
+        console.log(imageDerender)
 
-    console.log(div)
-    imageContainer.appendChild(div);
+    });
+
+    imageContainer.appendChild(input);
 }
 
 document.addEventListener('DOMContentLoaded', function() {
     var button = document.getElementById('submit');
-    // onClick's logic below:
+    var initial = true;
     button.addEventListener('click', function() {
-        // call the api
-        derenderImage();
-        // display results
-
+        
+        if (initial) {
+            derenderImage();
+            button.innerHTML = 'Submit!'
+            initial = false
+        }
+        else {
+            submitChanges()
+        }
     });
 });
 
